730eff0632882348cc0cc286aba224641a43163e
Emily <speckhalse@region18.org>
2019-01-20 22:23:30 -0500

    add talon id's

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -9,5 +9,17 @@ package frc.robot;
 
 
 public class RobotMap {
+
+    public static class DriveMap {
+        // TALON SRX ID'S (2018 bot, testbed)
+        public static final int ID_LEFT_FRONT = 2; // 2, 8
+		public static final int ID_LEFT_MIDDLE = 4; // 4, 11
+		public static final int ID_LEFT_REAR = 10; // 10, 5
+
+		public static final int ID_RIGHT_FRONT = 14; // 14, 7
+		public static final int ID_RIGHT_MIDDLE = 3; // 3, 12
+		public static final int ID_RIGHT_REAR = 9; // 9, 6
+
+    }
   
 }
