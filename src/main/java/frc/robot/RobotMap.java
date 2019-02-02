1b4a9720ab2ecd46cc0c193fc9896abcbf30bf7b
Emily <speckhalse@region18.org>
2019-02-02 11:02:08 -0500

    add Victor SPX follower id's

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -11,14 +11,22 @@ package frc.robot;
 public class RobotMap {
 
     public static class DriveMap {
-        // TALON SRX ID'S (2018 bot, testbed)
-        public static final int ID_LEFT_FRONT = 2; // 2, 8
-		public static final int ID_LEFT_MIDDLE = 4; // 4, 11
-		public static final int ID_LEFT_REAR = 10; // 10, 5
+        // TALON/VICTOR ID'S (2018 bot, testbed)
+		public static final int ID_LEFT_FRONT = 2; // 2, 8
+		public static final int ID_LEFT_MIDDLE = 4;
+		public static final int ID_LEFT_REAR = 3;
 
 		public static final int ID_RIGHT_FRONT = 14; // 14, 7
-		public static final int ID_RIGHT_MIDDLE = 3; // 3, 12
-		public static final int ID_RIGHT_REAR = 9; // 9, 6
+		public static final int ID_RIGHT_MIDDLE = 1;
+		public static final int ID_RIGHT_REAR = 2;
+
+
+		// old talons
+		// public static final int ID_LEFT_MIDDLE = 4; // 4, 11
+		// public static final int ID_LEFT_REAR = 10; // 10, 5
+
+		// public static final int ID_RIGHT_MIDDLE = 3; // 3, 12
+		// public static final int ID_RIGHT_REAR = 9; // 9, 6
 
 	}
 	
