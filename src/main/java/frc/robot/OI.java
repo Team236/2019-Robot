48a842df2580def62bdf5e15d2e96b98303195d8
Emily <speckhalse@region18.org>
2019-02-12 21:49:00 -0500

    semi-final buttons

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -12,6 +12,7 @@ import frc.robot.commands.camera.TiltFrontCamera;
 import frc.robot.commands.camera.TiltRearCamera;
 import frc.robot.commands.cargo.CargoEject;
 import frc.robot.commands.cargo.CargoExtend;
+import frc.robot.commands.cargo.CargoExtendAndRetract;
 import frc.robot.commands.cargo.CargoIntake;
 import frc.robot.commands.cargo.CargoRetract;
 import frc.robot.commands.drive.DriveWithThumbsticks;
@@ -35,9 +36,7 @@ public class OI {
   public Thrustmaster leftStick, rightStick;
   public LogitechF310 controller;
 
-  public OI() {
-    // TODO: "FINALIZE" BUTTONS
-    
+  public OI() {   
     leftStick = new Thrustmaster(RobotMap.JoystickMap.USB_LEFT);
     rightStick = new Thrustmaster(RobotMap.JoystickMap.USB_RIGHT);
     controller = new LogitechF310(RobotMap.JoystickMap.USB_CONTROLLER);
@@ -46,39 +45,42 @@ public class OI {
     controller.rb.whileHeld(new DriveWithThumbsticks());
 
     // POGO
-    // controller.back.whileHeld(new PogoWithThumbstick());
+    // TODO: button for auto endgame
+    controller.back.whileHeld(new PogoWithThumbstick());
     // controller.y.whenPressed(new PogoExtend());
     // controller.b.whenPressed(new PogoRetract());
     // controller.lb.whileHeld(new Roll());
 
     // CARGO
-    // controller.a.whileHeld(new CargoIntake());
-    // controller.b.whileHeld(new CargoEject());
-
-    // controller.y.whenPressed(new CargoRetract()); // up
-    // controller.x.whenPressed(new CargoExtend()); // down
+    leftStick.middle.whileHeld(new CargoIntake());
+    leftStick.trigger.whileHeld(new CargoEject());
+    controller.lb.toggleWhenPressed(new CargoExtendAndRetract());
 
     // HATCH
-    // controller.x.whileHeld(new HatchScore());
-    // controller.rb.whenPressed(new HatchExtendAndRetract());
-    // controller.a.whenPressed(new HatchExtend());
-    // controller.b.whenPressed(new HatchRetract());
+    rightStick.trigger.whenPressed(new HatchScore());
+    controller.rb.whenPressed(new HatchExtendAndRetract());
 
     // ELEVATOR
     controller.start.whileHeld(new ElevatorWithThumbstick());
-    controller.x.whenPressed(new ElevatorToHeight(56, 3, RobotMap.ElevatorMap.UP_PARAMS, RobotMap.ElevatorMap.DOWN_PARAMS));
-    controller.y.whenPressed(new ElevatorToHeight(28, 3, RobotMap.ElevatorMap.UP_PARAMS, RobotMap.ElevatorMap.DOWN_PARAMS));
+    // H1, cargo intake, C1 (rocket) in cargo mode
+    controller.a.whenPressed(new ElevatorToHeight(0, RobotMap.ElevatorMap.HEIGHT_MARGIN, RobotMap.ElevatorMap.UP_PARAMS, RobotMap.ElevatorMap.DOWN_PARAMS));
+    // only used in cargo mode, C1.5 (cargo ship)
+    controller.b.whenPressed(new ElevatorToHeight(12.5, RobotMap.ElevatorMap.HEIGHT_MARGIN, RobotMap.ElevatorMap.UP_PARAMS, RobotMap.ElevatorMap.DOWN_PARAMS));
+    // H2, C2 in cargo mode
+    controller.x.whenPressed(new ElevatorToHeight(28, RobotMap.ElevatorMap.HEIGHT_MARGIN, RobotMap.ElevatorMap.UP_PARAMS, RobotMap.ElevatorMap.DOWN_PARAMS));
+    // H3, C3 in cargo mode
+    controller.y.whenPressed(new ElevatorToHeight(56, RobotMap.ElevatorMap.HEIGHT_MARGIN, RobotMap.ElevatorMap.UP_PARAMS, RobotMap.ElevatorMap.DOWN_PARAMS));
 
 
-    // CAMERA
+    // CAMERA - rotating on servo
     JoystickPOV raiseFrontCam = new JoystickPOV(rightStick, Direction.UP);
     JoystickPOV lowerFrontCam = new JoystickPOV(rightStick, Direction.DOWN);
     JoystickPOV raiseRearCam = new JoystickPOV(leftStick, Direction.UP);
     JoystickPOV lowerRearCam = new JoystickPOV(leftStick, Direction.DOWN);
-    // raiseFrontCam.whenPressed(new TiltFrontCamera(0.0));
-    // lowerFrontCam.whenPressed(new TiltFrontCamera(1.0));
-    // raiseRearCam.whenPressed(new TiltRearCamera(0.0));
-    // lowerRearCam.whenPressed(new TiltRearCamera(1.0));
+    raiseFrontCam.whenPressed(new TiltFrontCamera(0.0));
+    lowerFrontCam.whenPressed(new TiltFrontCamera(1.0));
+    raiseRearCam.whenPressed(new TiltRearCamera(0.0));
+    lowerRearCam.whenPressed(new TiltRearCamera(1.0));
 
   }
 
