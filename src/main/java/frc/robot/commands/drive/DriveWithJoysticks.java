ac18636dab922fecde0e24709cdcc4cf7a7b3c11
Emily <speckhalse@region18.org>
2019-02-14 22:04:43 -0500

    add logic to slow drive speed when elevator is up

--- src/main/java/frc/robot/commands/drive/DriveWithJoysticks.java
+++ src/main/java/frc/robot/commands/drive/DriveWithJoysticks.java
@@ -21,12 +21,22 @@ public class DriveWithJoysticks extends Command {
 
   @Override
   protected void execute() {
-    if (Robot.oi.leftStick.left.get()) {
-      Robot.drive.setLeftSpeed(Robot.oi.leftStick.getY());
-      Robot.drive.setRightSpeed(Robot.oi.rightStick.getY());
+    if (Robot.elevator.getHeight() < 50) {
+      if (Robot.oi.leftStick.left.get()) {
+        Robot.drive.setLeftSpeed(Robot.oi.leftStick.getY());
+        Robot.drive.setRightSpeed(Robot.oi.rightStick.getY());
+      } else {
+        Robot.drive.setLeftSpeed(-Robot.oi.leftStick.getY());
+        Robot.drive.setRightSpeed(-Robot.oi.rightStick.getY());
+      }
     } else {
-      Robot.drive.setLeftSpeed(-Robot.oi.leftStick.getY());
-      Robot.drive.setRightSpeed(-Robot.oi.rightStick.getY());
+      if (Robot.oi.leftStick.left.get()) {
+        Robot.drive.setLeftSpeed(Robot.oi.leftStick.getY() * .3);
+        Robot.drive.setRightSpeed(Robot.oi.rightStick.getY() * .3);
+      } else {
+        Robot.drive.setLeftSpeed(-Robot.oi.leftStick.getY() * .3);
+        Robot.drive.setRightSpeed(-Robot.oi.rightStick.getY() * .3);
+      }
     }
   }
 
