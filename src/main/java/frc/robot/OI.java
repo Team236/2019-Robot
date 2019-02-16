3b2cdc43526e03c8a2e294fdeb91921e42385ff7
Emily <speckhalse@region18.org>
2019-02-16 17:32:16 -0500

    add TwoButton's for cargo heights

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -8,6 +8,7 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.robot.commands.auto.GyroDrive;
 import frc.robot.commands.camera.TiltFrontCamera;
 import frc.robot.commands.camera.TiltRearCamera;
 import frc.robot.commands.cargo.CargoEject;
@@ -29,6 +30,7 @@ import frc.robot.commands.pogo.Roll;
 import lib.oi.LogitechF310;
 import lib.oi.Thrustmaster;
 import lib.oi.triggers.JoystickPOV;
+import lib.oi.triggers.TwoButton;
 import lib.oi.triggers.JoystickPOV.Direction;
 import lib.pid.PIDParameters;
 
@@ -43,13 +45,14 @@ public class OI {
 
     // DRIVE
     controller.rb.whileHeld(new DriveWithThumbsticks());
+    rightStick.right.whileHeld(new GyroDrive(RobotMap.AutoMap.GYRO_DRIVE_KP, 240, .25));
 
     // POGO
     // TODO: button for auto endgame
-    controller.back.whileHeld(new PogoWithThumbstick());
-    // controller.y.whenPressed(new PogoExtend());
-    // controller.b.whenPressed(new PogoRetract());
-    // controller.lb.whileHeld(new Roll());
+    // controller.back.whileHeld(new PogoWithThumbstick()); // right is forward
+    // controller.y.whileHeld(new PogoExtend());
+    // controller.b.whileHeld(new PogoRetract());
+    // controller.a.whileHeld(new Roll());
 
     // CARGO
     leftStick.middle.whileHeld(new CargoIntake());
@@ -58,19 +61,26 @@ public class OI {
 
     // HATCH
     rightStick.trigger.whenPressed(new HatchScore());
-    controller.rb.whenPressed(new HatchExtendAndRetract());
+    // controller.a.whenPressed(new HatchExtend());
+    // controller.b.whenPressed(new HatchRetract());
+    controller.rb.toggleWhenPressed(new HatchExtendAndRetract());
 
     // ELEVATOR
-    controller.start.whileHeld(new ElevatorWithThumbstick());
+    // controller.start.whileHeld(new ElevatorWithThumbstick());
+    TwoButton cargo1 = new TwoButton(controller.start, controller.a);
+    TwoButton cargo2 = new TwoButton(controller.start, controller.x);
+    TwoButton cargo3 = new TwoButton(controller.start, controller.y);
     // H1, cargo intake, C1 (rocket) in cargo mode
+    cargo1.whenPressed(new ElevatorToHeight(0 + RobotMap.ElevatorMap.CARGO_OFFSET, RobotMap.ElevatorMap.HEIGHT_MARGIN, RobotMap.ElevatorMap.UP_PARAMS, RobotMap.ElevatorMap.DOWN_PARAMS));
     controller.a.whenPressed(new ElevatorToHeight(0, RobotMap.ElevatorMap.HEIGHT_MARGIN, RobotMap.ElevatorMap.UP_PARAMS, RobotMap.ElevatorMap.DOWN_PARAMS));
     // only used in cargo mode, C1.5 (cargo ship)
     controller.b.whenPressed(new ElevatorToHeight(12.5, RobotMap.ElevatorMap.HEIGHT_MARGIN, RobotMap.ElevatorMap.UP_PARAMS, RobotMap.ElevatorMap.DOWN_PARAMS));
     // H2, C2 in cargo mode
+    cargo2.whenPressed(new ElevatorToHeight(28 + RobotMap.ElevatorMap.CARGO_OFFSET, RobotMap.ElevatorMap.HEIGHT_MARGIN, RobotMap.ElevatorMap.UP_PARAMS, RobotMap.ElevatorMap.DOWN_PARAMS));
     controller.x.whenPressed(new ElevatorToHeight(28, RobotMap.ElevatorMap.HEIGHT_MARGIN, RobotMap.ElevatorMap.UP_PARAMS, RobotMap.ElevatorMap.DOWN_PARAMS));
     // H3, C3 in cargo mode
+    cargo3.whenPressed(new ElevatorToHeight(56 + RobotMap.ElevatorMap.CARGO_OFFSET, RobotMap.ElevatorMap.HEIGHT_MARGIN, RobotMap.ElevatorMap.UP_PARAMS, RobotMap.ElevatorMap.DOWN_PARAMS));
     controller.y.whenPressed(new ElevatorToHeight(56, RobotMap.ElevatorMap.HEIGHT_MARGIN, RobotMap.ElevatorMap.UP_PARAMS, RobotMap.ElevatorMap.DOWN_PARAMS));
-
 
     // CAMERA - rotating on servo
     JoystickPOV raiseFrontCam = new JoystickPOV(rightStick, Direction.UP);
@@ -85,3 +95,4 @@ public class OI {
   }
 
 }
+        
