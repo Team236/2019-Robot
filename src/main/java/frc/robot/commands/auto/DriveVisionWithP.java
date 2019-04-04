2ae518fcbca1f154aa6ab73f8c1e862327c3d2e8
nonbrewer <nonbrewer@users.noreply.github.com>
2019-04-04 15:59:39 -0400

    Change Gyro to DriveVisionWithP with two speeds
    New DriveVisionWithP class includes Limelight steering with additional capability half speed when close to target

--- src/main/java/frc/robot/commands/auto/DriveVisionWithP.java
+++ src/main/java/frc/robot/commands/auto/DriveVisionWithP.java
@@ -11,47 +11,52 @@ import edu.wpi.first.wpilibj.command.Command;
 import frc.robot.Robot;
 import frc.robot.RobotMap;
 
-public class GyroDrive extends Command {
+public class DriveVisionWithP extends Command {
   private double kP;
-  private double dist;
   private double speed;
 
-  public GyroDrive(double _kP, double _dist, double _speed) {
+  public DriveVisionWithP(double _kP, double _speed) {
     this.kP = _kP;
-    this.dist = _dist;
     this.speed = _speed;
 
     requires(Robot.drive);
+   // if more than one method will use LimeLight requires should be defined
+   // requires(Robot.theLimeLight);
   }
 
   @Override
   protected void initialize() {
-    Robot.drive.navx.reset();
     Robot.drive.resetEncoders();
     // System.out.println("gyro drive init");
   }
 
   @Override
   protected void execute() {
+    
     double leftSpeed = speed;
     double rightSpeed = speed;
-
-    double ang = Robot.drive.navx.getAngle();
-
-    leftSpeed -= ang * kP;
-    rightSpeed += ang * kP;
+    
+    double ang = Robot.theLimeLight.getLimeLight().getdegRotationToTarget();
+    
+  //TODO need to determine if 0.3 is a valid area by positioning robot 3-4 feet from target
+    double area = Robot.theLimeLight.getLimeLight().getTargetArea();
+    
+    if (area > .3) {  
+      leftSpeed += ang * kP * .5;   // 50% reduced speed when target is close
+      rightSpeed -= ang * kP * .5;  // 50% reduced speed when target is close
+    } else {
+      leftSpeed += ang * kP;
+      rightSpeed -= ang * kP;
+    }
 
     Robot.drive.setLeftSpeed(leftSpeed);
     Robot.drive.setRightSpeed(rightSpeed);
-    // System.out.println("gyro drive exec");
+    // System.out.println("driveVisionWithP");
   }
 
   @Override
   protected boolean isFinished() {
-    boolean isLeftFinished = Math.abs(Robot.drive.getLeftDist() - dist) < RobotMap.AutoMap.MARGIN_GYRO_DRIVE;
-    boolean isRightFinished = Math.abs(Robot.drive.getRightDist() - dist) < RobotMap.AutoMap.MARGIN_GYRO_DRIVE;
-
-    return isLeftFinished && isRightFinished;
+    return false;
   }
 
   @Override
